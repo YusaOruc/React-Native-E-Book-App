fba70e765720b157deb05387ebfce658d7f03764
YusaOruc <h.yusa98@gmail.com>
2024-04-07 15:38:40 +0300

    fix: setting page

--- views/settings/Settings.js
+++ views/settings/Settings.js
@@ -1,74 +1,90 @@
 import React from "react";
-import { View, Text, StyleSheet, Switch, TouchableOpacity } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  SectionList,
+  StatusBar,
+} from "react-native";
+import { SafeAreaViewComponent } from "../../components/SafeAreaViewComponent";
+import { TextComponent } from "../../components/TextComponent";
+import { Ionicons } from "@expo/vector-icons";
+import useTheme from "../../hooks/useTheme";
+import { useGlobalState } from "../../context/GlobalStateContext";
+
+const DATA = [
+  {
+    title: "Main dishes",
+    data: ["Pizza", "Burger", "Risotto"],
+  },
+  {
+    title: "Sides",
+    data: ["French Fries", "Onion Rings", "Fried Shrimps"],
+  },
+];
 
 export default function Settings() {
-  const [notificationsEnabled, setNotificationsEnabled] = React.useState(false);
-  const [darkModeEnabled, setDarkModeEnabled] = React.useState(false);
-
-  const toggleNotifications = () => {
-    setNotificationsEnabled((prev) => !prev);
-  };
-
-  const toggleDarkMode = () => {
-    setDarkModeEnabled((prev) => !prev);
-  };
+  const { isDarkMode, toggleDarkMode } = useGlobalState();
 
   return (
-    <View style={styles.container}>
-      <View style={styles.section}>
-        <Text style={styles.sectionTitle}>Bildirimler</Text>
-        <Switch
-          trackColor={{ false: "#767577", true: "#81b0ff" }}
-          thumbColor={notificationsEnabled ? "#f5dd4b" : "#f4f3f4"}
-          ios_backgroundColor="#3e3e3e"
-          onValueChange={toggleNotifications}
-          value={notificationsEnabled}
+    <SafeAreaViewComponent style={styles.container}>
+      <View
+        style={{
+          width: "100%",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Image
+          source={{ uri: "https://i.pravatar.cc/200" }}
+          style={{
+            ...styles.avatar,
+            borderWidth: 2,
+            borderColor: "#D45555",
+          }}
         />
+        <TextComponent style={styles.title}>Jhon Doe</TextComponent>
       </View>
-
-      <View style={styles.section}>
-        <Text style={styles.sectionTitle}>Karanlık Mod</Text>
-        <Switch
-          trackColor={{ false: "#767577", true: "#81b0ff" }}
-          thumbColor={darkModeEnabled ? "#f5dd4b" : "#f4f3f4"}
-          ios_backgroundColor="#3e3e3e"
-          onValueChange={toggleDarkMode}
-          value={darkModeEnabled}
-        />
-      </View>
-
-      <TouchableOpacity style={styles.button}>
-        <Text style={styles.buttonText}>Çıkış Yap</Text>
-      </TouchableOpacity>
-    </View>
+      <SectionList
+        style={styles.section}
+        sections={DATA}
+        keyExtractor={(item, index) => item + index}
+        renderItem={({ item }) => (
+          <View style={styles.item}>
+            <Ionicons
+              name={"settings"}
+              size={24}
+              style={{ marginRight: 10 }}
+              color={isDarkMode ? "white" : "black"}
+            />
+            <TextComponent style={styles.title}>{item}</TextComponent>
+          </View>
+        )}
+        renderSectionHeader={({ section: { title } }) => (
+          <TextComponent style={styles.header}>{title}</TextComponent>
+        )}
+      />
+    </SafeAreaViewComponent>
   );
 }
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    paddingHorizontal: 20,
-  },
-  section: {
+  container: { justifyContent: "center" },
+  section: { padding: 15 },
+  item: {
+    padding: 15,
+    marginVertical: 2,
     flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-    marginBottom: 20,
   },
-  sectionTitle: {
+  header: {
+    fontSize: 12,
+  },
+  title: {
     fontSize: 18,
   },
-  button: {
-    backgroundColor: "#D45555",
-    paddingVertical: 12,
-    paddingHorizontal: 24,
-    borderRadius: 8,
-    marginTop: 20,
-  },
-  buttonText: {
-    color: "white",
-    fontSize: 16,
+  avatar: {
+    width: 150,
+    height: 150,
+    borderRadius: 75,
   },
 });
